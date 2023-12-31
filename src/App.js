c6ed8c4ab1ecf2f7fe36bf3dc389a7e0f5aca849
lorhan77 <gabriellorhan7@gmail.com>
2023-12-31 19:42:01 -0400

    Front inicio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
+import Cadastrar from './Pages/Cadastrar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Routes>
+        
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/cadastrar" element={<Cadastrar />} />
+         
+
+
+
+      </Routes>
+
+
+
+    </Router>
   );
 }
 
